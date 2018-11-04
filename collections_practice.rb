8b1757d0d48213af37ae500a0bebf0c500795b2a
Richard Yance <yance0022@gmail.com>
2018-11-04 23:34:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,13 @@ end
 def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.each do |kesha|
+    kesha = ke$ha 
+  end
+end
